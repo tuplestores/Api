f815114bdb4eb128b35d849db0595b26f7ee0399
ajishd <ajishd@192.168.1.101>
2019-05-20 15:43:39 +0530

    ser

--- dispatchAPI/src/main/java/com/tuplestores/api/service/AuthenticationService.java
+++ dispatchAPI/src/main/java/com/tuplestores/api/service/AuthenticationService.java
@@ -1,9 +1,10 @@
-//Commented by Ajish
+//Authentication Service
+//Written By Ajish Dharman
 package com.tuplestores.api.service;
 
 import com.tuplestores.api.model.general.User;
 
-//commented by ajish
+
 public interface AuthenticationService {
 
 	User SignIn(String email, String password); 
